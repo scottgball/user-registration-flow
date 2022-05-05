7a3a7226382a0713f133a7c6a9f0cd152cabe138
Scott Ball <scottgball@gmail.com>
2022-05-05 08:15:52 -0700

    Basic registration form, registers users. No form validation yet

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,10 +1,20 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="q-pa-md q-gutter-sm">
+     <q-btn color="primary" label="Register" v-if="!showRegistrationForm" @click="showRegistrationForm = true" />
+    </div>
+    <form>
+      <div class="q-pa-md" v-if="showRegistrationForm">
+        <div class="q-gutter-md" style="max-width: 300px">
+        <q-input outlined v-model="newUser.username" label="Username" />
+        <q-input outlined v-model="newUser.password" label="Password" />
+      </div>
+   </div>
+   <div class="q-pa-md q-gutter-sm registrationButtonsLayout" v-if="showRegistrationForm">
+     <q-btn color="primary" label="Submit" @click="addUser" />
+     <q-btn color="primary" label="Reset" @click="resetRegistrationForm" />
+    </div>
+  </form>
   </q-page>
 </template>
 
@@ -12,6 +22,24 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      showRegistrationForm: false,
+      users: [{}],
+      newUser: {username: '', password: ''},
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.newUser);
+      this.resetRegistrationForm();
+      this.showRegistrationForm = false;
+
+    },
+    resetRegistrationForm() {
+      this.newUser = {username: '', password: ''};
+    },
+  }
 })
 </script>
